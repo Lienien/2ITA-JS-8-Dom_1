d4a2d8cdd0900cc5a8461df0f6b1945970fa44e5
Lienien <116272320+Lienien@users.noreply.github.com>
2023-10-18 10:16:17 +0200

    bogos binted

---
+++ Oppgave 1/index.js
@@ -0,0 +1,27 @@
+//Oppgave 1
+
+const tempInput = document.getElementById("tempInput")
+const tofahrenheitbtn = document.getElementById("to-fahrenheit-btn")
+const tocelsiusbtn = document.getElementById("to-celsius-btn")
+const result = document.getElementById("result")
+
+function convertToCelsius(temp) {
+  return (temp - 32) * (5 / 9);
+}
+
+function convertToFahrenheit(temp) {
+  return temp * (9 / 5) + 32;
+}
+
+tocelsiusbtn.addEventListener("click", function () {
+  console.log("click");
+  const temp = parseFloat(tempInput.value);
+  const celsius = convertToCelsius(temp);
+  // result.textContent = `${temp} grader Fahrenheit tilsvarer ${celsius.toFixed(1)} grader Celsius.`;
+});
+
+tofahrenheitbtn.addEventListener("click", function () {
+  const temp = parseFloat(tempInput.value);
+  const fahrenheit = convertToFahrenheit(temp);
+  // result.textContent = `${temp} grader Celsius tilsvarer ${fahrenheit.toFixed(1)} grader Fahrenheit.`;
+});
